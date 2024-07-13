203b455ef178dcabc9d91e4908711560f6e772e8
Evandro Santos <vandersant7@gmail.com>
2024-07-13 18:59:34 -0300

    feat: create routes.ts

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getPlayer } from "./controllers/players-controller";
+
+const router = Router()
+router.get("/players", getPlayer)
+
+export default router

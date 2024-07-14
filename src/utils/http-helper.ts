8da859feb56c594877abb2249bc3e194018074fe
Evandro Santos <vandersant7@gmail.com>
2024-07-14 19:28:46 -0300

    feat: create implements statusCode badRequest, created and noContent

---
+++ src/utils/http-helper.ts
@@ -0,0 +1,35 @@
+import { HttpResponse } from "../models/http-response-model"
+
+
+export const ok = async (data: any): Promise<HttpResponse> => {
+    return {
+        
+        statusCode: 200,
+        body: data
+    }
+}
+
+export const created = async (): Promise<HttpResponse> => {
+    return {
+        statusCode: 201,
+        body: {
+            message: "successful"
+        }
+    }
+}
+
+export const noContent = async(): Promise<HttpResponse> => {
+    return {
+        statusCode: 204,
+        body: {
+            message: "successful"
+        },
+    }
+}
+
+export const badRequest = async(): Promise<HttpResponse> => {
+    return {
+        statusCode: 400,
+        body: null,
+    }
+}

e52b906084ef16dc86af916d3b3fde89425af09a
Evandro Santos <vandersant7@gmail.com>
2024-07-14 19:40:58 -0300

    feat: implements delete routes

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import * as PlayersController from "./controllers/players-controller";
 const router = Router()
 router.get("/players", PlayersController.getPlayer)
 router.post("/players", PlayersController.postPlayer)
+router.delete("/players/:id", PlayersController.deletePlayer)
 
 router.get("/players/:id", PlayersController.getPlayerById)
 // No exemplo acima estamos usando uma router param "parametro de rota". Depois da "/" utilizamos ":" + o nome do parametro que iremos passar

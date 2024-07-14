60f36539c75756d18c1eb62f2ef715ba3e91e071
Evandro Santos <vandersant7@gmail.com>
2024-07-14 19:30:48 -0300

    feat: create players controller

---
+++ src/controllers/players-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express'
+import * as service from '../services/players-services'
+import { noContent } from '../utils/http-helper'
+
+export const getPlayer = async (req: Request, res: Response) => {
+  // Vamos criar uma chamada para o nosso service
+  const HttpResponse = await service.getPlayerDataService()
+
+  res.status(HttpResponse.statusCode).json(HttpResponse.body)
+}
+
+export const getPlayerById = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+
+  const httpResponse = await service.getPlayerByIdService(id)
+  res.status(httpResponse.statusCode).json(httpResponse.body)
+}
+
+export const postPlayer = async (req: Request, res: Response) => {
+  const bodyValue = req.body
+
+  const httpResponse = await service.createPlayerService(bodyValue)
+  
+  if (httpResponse) {
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+  //else{
+  //   const response = noContent()
+  //   res.status((await response).statusCode).json((await response).body)
+  // }
+}

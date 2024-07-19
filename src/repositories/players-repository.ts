e73c4d7bb2a876f73b0c652f7f5ecb1efee018dd
Evandro Santos <vandersant7@gmail.com>
2024-07-19 16:05:30 -0300

    feat: function findAndModifyPlayer

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,4 +1,5 @@
 import { PlayerModel } from '../models/player-model'
+import { StatisticsModel } from '../models/statistics-model'
 
 const database: PlayerModel[] = [
   {
@@ -102,5 +103,24 @@ export const deleteOnePlayer = async (id: number) => {
 
   if (index !== -1) {
     database.splice(index, 1)
+    return true
   }
+
+  return false
+}
+
+export const findAndModifyPlayer = async (
+  id: number,
+  statistics: StatisticsModel
+): Promise<PlayerModel> => {
+
+
+  //find player
+  const playerIndex = database.findIndex((player) => player.id === id)
+
+  if (playerIndex !== -1) {
+    database[playerIndex].statistics = statistics
+  }
+
+  return database[playerIndex] 
 }

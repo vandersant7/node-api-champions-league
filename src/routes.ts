7d27fec4d45f113dd2bb23128496e231c99512d0
Evandro Santos <vandersant7@gmail.com>
2024-07-19 16:07:01 -0300

    fix: routes

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ const router = Router()
 router.get("/players", PlayersController.getPlayer)
 router.post("/players", PlayersController.postPlayer)
 router.delete("/players/:id", PlayersController.deletePlayer)
+router.patch("/players/:id", PlayersController.updatePlayers)
 
 router.get("/players/:id", PlayersController.getPlayerById)
 // No exemplo acima estamos usando uma router param "parametro de rota". Depois da "/" utilizamos ":" + o nome do parametro que iremos passar

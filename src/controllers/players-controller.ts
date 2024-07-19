d72102ef405063f21216cbe2723523a5feb2adf8
Evandro Santos <vandersant7@gmail.com>
2024-07-19 16:06:40 -0300

    feat: create updatePlayers

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import * as service from '../services/players-services'
-import { noContent } from '../utils/http-helper'
+import { StatisticsModel } from '../models/statistics-model'
 
 export const getPlayer = async (req: Request, res: Response) => {
   // Vamos criar uma chamada para o nosso service
@@ -20,7 +20,7 @@ export const postPlayer = async (req: Request, res: Response) => {
   const bodyValue = req.body
 
   const httpResponse = await service.createPlayerService(bodyValue)
-  
+
   if (httpResponse) {
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
@@ -33,5 +33,13 @@ export const postPlayer = async (req: Request, res: Response) => {
 export const deletePlayer = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id)
   const httpResponse = await service.deletePlayerService(id)
+
+  res.status(httpResponse.statusCode).json(httpResponse.body)
 }
 
+export const updatePlayers = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+  const bodyValue: StatisticsModel = req.body
+  const httpResponse = await service.updatePlayerService(id, bodyValue)
+  res.status(httpResponse.statusCode).json(httpResponse.body)
+}

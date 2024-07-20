278fda2e3c899b12ec520cd55556e74cc6a5dfa6
Evandro Santos <vandersant7@gmail.com>
2024-07-19 23:48:15 -0300

    feat: implements clubs service

---
+++ src/services/club-service.ts
@@ -0,0 +1,10 @@
+import * as HttpResponse from "../utils/http-helper"
+import * as repository from "../repositories/clubs-repository"
+
+export const getClubService = async () => {
+    const data = await repository.findAllClubs()
+
+    const response = HttpResponse.ok(data)
+
+    return response;
+};

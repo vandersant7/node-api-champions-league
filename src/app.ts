154376b7cce17282f38f299b911443bf17f3cf1f
Evandro Santos <vandersant7@gmail.com>
2024-07-19 23:52:22 -0300

    feat: install and implements cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 
 function createApp() {
   const app = express()
 
   // Esse middleware define que o conteúdo será um json
   app.use(express.json())
+  app.use('/api', router)
 
-  app.use("/api", router)
+  //Essas configurações definem que pode acessar minhas API, se tiver mais de um cliente consumindo a API, as propriedades origin e methods podem ser coladas dentro de um vetor.
+  // const corsOption = {
+  //   origin:['http://evandro.developer.dev', 'http://gov.br'],
+  //   methods: ['GET', 'UPDATE'],
+  // }
+  // app.use(cors(corsOption))
+  app.use(cors)
 
   return app
 }
